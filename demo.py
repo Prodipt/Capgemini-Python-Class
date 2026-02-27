5bf7553feca09908f34a9f5cca58337405dc2831
prasoon <prasoonpradipt2003@gmail.com>
2026-02-27 11:47:00 +0530

    27-02-2026

---
+++ demo.py
@@ -0,0 +1,84 @@
+# a=list(input('Enter a value : '))
+# print(a+500)
+
+#11
+# n = int(input('Enter a value! '))
+
+# if n>100:
+#     print(True)
+# else:
+#     print(False)
+
+# a = input('Enter a value! ')
+
+# s = a[::-1]
+# print(s)
+# if s==a:
+#     print(True)
+# else:
+#     print(False)  
+
+# a = int(input('Enter a value! '))
+
+# if a>40:
+#     print("HOT")
+# else:
+#     print("Cold")  
+
+
+# name= "PP"
+# password = "idk"
+
+# n = input('Enter a name: ')
+# p = input('Enter a password: ')
+
+# if n==name and password==p:
+#     print(True)
+# else:
+#     print(False)
+
+
+# a = int(input('Enter a value! '))
+# b = int(input('Enter a value! '))
+
+# print("sum ", a+b)
+# print("sub ", abs(a-b))
+
+
+# a = int(input('Enter a value! '))
+
+# if a>10 and a<50:
+#     print("in range")
+# else:
+#     print("not in range")  
+
+# a = int(input('Enter a value! '))
+
+# if a>=100 and a<1000:
+#     print("Yes It is")
+# else:
+#     print("Not a 3-digit number")  
+
+# a = input('Enter a value! ')
+
+# if len(a)>5 :
+#     print("Yes It is")
+# else:
+#     print("length less than 5")  
+
+# a = int(input('Enter a value! '))
+
+# if a==0:
+#     print("Yes It is")
+# else:
+#     print("Not a zero value")  
+
+Id= "xyz"
+
+id = input('Enter a id: ')
+age = int(input('Enter a age: '))
+
+if id==Id and age>=18:
+    print("Yes, Go for it, You are Eligible")
+else:
+    print("Not Eligible!")
