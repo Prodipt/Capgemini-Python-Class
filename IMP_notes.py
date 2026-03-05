e12a5ce2b034bddcbc1af2febd267fe0328b170b
prasoon <prasoonpradipt2003@gmail.com>
2026-03-05 14:40:55 +0530

    Notes

---
+++ IMP_notes.py
@@ -0,0 +1,14 @@
+# 05-03-2026 (Thursday)
+
+# FUNCTIONS
+# In Built Functions
+'''
+-> Utility in-built function
+-> String functions: upper, lower, isupper, islower, swapcase, replace, capitalize, title, split, join, index, count, isdigit, strip, rstrip, lstrip
+-> List functions: sort, append, insert, pop, remove, reverse, count, index
+-> Tuple functions: index, count
+-> Set functions: update, remove, pop, add, union, intersaction, difference, clear
+-> Dictionary functions: values, items, pop, update, clear
+'''
+
+# User Defined Functions
