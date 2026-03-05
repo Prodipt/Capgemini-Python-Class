514725c6187c552bbec3624b90391d2630d23e68
prasoon <prasoonpradipt2003@gmail.com>
2026-03-05 14:52:52 +0530

    Exercises

---
+++ exercise.py
@@ -0,0 +1,111 @@
+# n = int(input('Enter a value! '))
+
+# if n>100:
+#     print(True)
+# else:
+#     print(False)
+
+# a = input('Enter a value! ')
+
+# s = a[::-1]
+# print(s)
+# if s==a:
+#     print(True)
+# else:
+#     print(False)  
+
+# a = int(input('Enter a value! '))
+
+# if a>40:
+#     print("HOT")
+# else:
+#     print("Cold")  
+
+
+# name= "PP"
+# password = "idk"
+
+# n = input('Enter a name: ')
+# p = input('Enter a password: ')
+
+# if n==name and password==p:
+#     print(True)
+# else:
+#     print(False)
+
+
+# a = int(input('Enter a value! '))
+# b = int(input('Enter a value! '))
+
+# print("sum ", a+b)
+# print("sub ", abs(a-b))
+
+
+# a = int(input('Enter a value! '))
+
+# if a>10 and a<50:
+#     print("in range")
+# else:
+#     print("not in range")  
+
+# a = int(input('Enter a value! '))
+
+# if a>=100 and a<1000:
+#     print("Yes It is")
+# else:
+#     print("Not a 3-digit number")  
+
+# a = input('Enter a value! ')
+
+# if len(a)>5 :
+#     print("Yes It is")
+# else:
+#     print("length less than 5")  
+
+# a = int(input('Enter a value! '))
+
+# if a==0:
+#     print("Yes It is")
+# else:
+#     print("Not a zero value")  
+
+# Id= "xyz"
+
+# id = input('Enter a id: ')
+# age = int(input('Enter a age: '))
+
+# if id==Id and age>=18:
+#     print("Yes, Go for it, You are Eligible")
+# else:
+#     print("Not Eligible!")
+
+
+# age = int(input("Enter age"))
+# income = int(input("Enter income"))
+# credit = int(input("Enter credit"))
+
+
+# if age>=21:
+#     if income >=2500:
+#         if credit >=700:
+#             print("Loan Approved!")
+#         else:
+#             print("Loan Rejected (Low Credit Score)")
+#     else:
+#         print("Loan Rejected (Low income)")
+# else:
+#     print("Loan Rejected (Age not Eligible)")
+
+
+# cpp = int(input("Enter cpp: "))
+# python = int(input("Enter python: "))
+# DSA = int(input("Enter DSA: "))
+
+# if cpp>=40 and python>=40 and DSA>=40:
+#     avg = (cpp+python+DSA)/3
+#     if avg >=75:
+#         print('Distinction!!!')
+#     else:
+#         print('Pass!!!')
+# else:
+#     print('Fail!')
