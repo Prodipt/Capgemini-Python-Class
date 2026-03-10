5dfb6701ea7152152745899bb15214d16352025e
prasoon <prasoonpradipt2003@gmail.com>
2026-03-10 12:51:15 +0530

    10th March

---
+++ (10th March)Inheritence.py
@@ -0,0 +1,65 @@
+
+# Inheritence
+
+# class kapdo:
+#     def __init__(self, color, type):
+#         self.color = color
+#         self.type = type
+    
+#     def info(self):
+#         print(self.color, self.type)
+
+# class dukan1(kapdo):
+#     def xyz(self):
+#         print("self service")
+
+
+
+# d = dukan1("pink","Tanki")
+# d.info()
+
+
+# class Employee:
+#     def __init__(self, name, salary):
+#         self.name = name
+#         self.salary = salary
+
+#     def info(self):
+#         print("Name of Manager : ", self.name)
+#         print("Salary of Manager : ", self.salary)
+
+
+# class Manager(Employee):
+#     def __init__(self, name, salary, department):
+#         super().__init__(name, salary)
+#         self.department = department
+
+#     def whichdept(self):
+#         print("Deapartment Name: ", self.department)
+
+#     @property
+#     def okok(self):
+#         print("Happy IDK!!!")
+
+
+# M1 = Manager("ABC", 2000, "CSE")
+# M1.info()
+# M1.whichdept()
+# M1.okok
+
+
+
+# class Circle:
+#     def __init__(self):
+#         self.radius = int(input("Enter the radius: "))
+#         print(f"Radius Of Circle : {self.radius}")
+
+#     def area(self):
+#         print("Area of Circle : ", (3.14)*self.radius*self.radius)
+
+#     def circumference(self):
+#         print("Circumference of Circle : ", 2*(3.14)*self.radius)
+
+# c1 = Circle()
+# c1.area()
+# c1.circumference()
--- (9th March)Oops.py
+++ (9th March)Oops.py
@@ -1,13 +1,13 @@
 # # 'Date - 9th March 2026'
 
-class College:
-    c_name = 'JECRC'
-    loc = 'Jaipur'
+# class College:
+#     c_name = 'JECRC'
+#     loc = 'Jaipur'
 
-    def __init__(self, name, id, age):
-        self.name = name
-        self.id = id
-        self.age = age
+#     def __init__(self, name, id, age):
+#         self.name = name
+#         self.id = id
+#         self.age = age
 
 
 # s1 = College()
@@ -24,18 +24,38 @@ class College:
 # s2 = College
 # print(s2.name)
 
-class Corporate:
-    Company = 'XYZ'
-    networth = 100000
-    started = "9thMarch"
+# class Corporate:
+#     Company = 'XYZ'
+#     networth = 100000
+#     started = "9thMarch"
 
-    def __init__(self, id, branch, qualification):
-        self.id = id
-        self.branch = branch
-        self.qualification = qualification
+#     # Creating Constructor
+#     def __init__(self, id, branch, qualification):
+#         self.id = id
+#         self.branch = branch
+#         self.qualification = qualification
 
-    def display(self):
-        print(self.id, self.branch, self.qualification)
+#     # Creating Object Method
+#     def display(self):
+#         print(self.id, self.branch, self.qualification)
+
+#     # Creating class Method
+#     @classmethod
+#     def c_display(cls):
+#         print(cls.Company,cls.networth, sep='  ')
+
+#     @staticmethod
+#     def add(a,d):
+#         return a+d
+
+# print(Corporate.add(2,5))
+
+# Corporate.c_display()
+
+# c1= Corporate
+
+# print(c1.add(2,8))
+# c1.c_display()
 
 # E1 = Corporate
 # E1.id = 101
@@ -69,7 +89,38 @@ class Corporate:
 # print(E3.branch)
 # print(E3.qualification)
 
-E4=Corporate(404, "Tester", "Python")
-E5=Corporate(505, "Trainee", "Python")
-E4.display()
-E5.display()
+# E4=Corporate(404, "Tester", "Python")
+# E5=Corporate(505, "Trainee", "Python")
+# E4.display()
+# E5.display()
+
+
+# withdraw, deposit, print balance, create account-> edit constructor
+# class Account:
+#     def __init__(self, name, balance=0):
+#         self.name = name
+#         self.balance = balance
+#         print(f"Account created for {self.name} with balance {self.balance}.")
+
+#     def deposit(self, amount):
+#         if amount >= 0:
+#             self.balance += amount
+#             print(f"Successfully deposited amount: {amount}. Current Balance: {self.balance}.")
+#         else:
+#             print("Invalid amount entered.")
+
+#     def withdraw(self, amount):
+#         if amount < 0:
+#             print("Invalid amount entered.")
+#         elif amount > self.balance:
+#             print("Insufficient balance.")
+#         else:
+#             self.balance -= amount
+#             print(f"Withdrawn: {amount}. Current balance: {self.balance}")
+#     def balance(self):
+#         print(f"Current Balance: {self.balance}")
+
+# a1 = Account("Haina Kaval", 2000)
+# a2 = Account("XYZ class", 2000)
+# a3 = Account("Aara rara", 2000)
+
